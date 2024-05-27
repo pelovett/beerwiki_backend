6f9ab45ca810ce61f71227c9d01fe1fc922cb8e6
Evan <evankender503@gmail.com>
2024-05-27 15:59:16 -0700

    added serveraddress env var

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,12 +1,16 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		server_address := os.Getenv("SERVER_ADDRESS")
+		println(server_address)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", server_address)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Set-Cookie")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
